8a91edbc8b5324b45af8cee7d3dea354246ab62f
Caterina <categalafassi@gmail.com>
2025-01-28 12:53:09 -0300

    entrega

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="Paquete_CaterinaGalafassi_SegundaEntrega",
+    version='0.0.1',
+    author="Caterina Galafassi",
+    author_email="categalafassi@gmail.com",
+    description="Segunda entrega del curso inicial de Python de Coderhouse",
+    packages=["CaterinaGalafassi_SegundaEntrega"]
+)
